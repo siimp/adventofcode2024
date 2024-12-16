9ceedbd370f9daabb584a1dead9d888855f83256
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2024-12-17 01:45:55 +0200

    day 5 part 2

---
+++ day5/part2.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "strings"
+    "slices"
+    "strconv"
+)
+
+func main() {
+    file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	result := 0
+    pagesBefore := make(map[string][]string)
+
+    rulesSection := true
+    scanner := bufio.NewScanner(bufio.NewReader(file))
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        if line == "" {
+            rulesSection = false
+            continue
+        }
+
+        if rulesSection {
+            parseRule(line, pagesBefore)
+        } else {
+            pages := strings.Split(line, ",")
+            isValid := isValidLine(pages, pagesBefore)
+            if !isValid {
+                correctedPages := correctPageOrder(pages, pagesBefore)
+                pageNumber, _ := strconv.Atoi(correctedPages[len(correctedPages)/2])
+                result += pageNumber
+            }
+        }
+    }
+
+	fmt.Printf("Result: %d\n", result)
+}
+
+func correctPageOrder(pages []string, pagesBefore map[string][]string) ([]string) {
+    correctPages := []string{}
+
+    for _, page := range pages {
+        if len(correctPages) == 0 {
+            correctPages = append(correctPages, page)
+            continue
+        }
+
+        before := pagesBefore[page]
+        inserted := false
+        for correctIndex, correctPage := range correctPages {
+            if slices.Contains(before, correctPage) {
+                correctPages = slices.Insert(correctPages, correctIndex, page)
+                inserted = true
+                break
+            }
+        }
+
+        if !inserted {
+            correctPages = append(correctPages, page)
+        }
+
+    }
+
+    return correctPages
+}
+
+func parseRule(line string, pagesBefore map[string][]string) {
+    numbers := strings.Split(line, "|")
+    pagesBefore[numbers[0]] = append(pagesBefore[numbers[0]], numbers[1])
+}
+
+func isValidLine(pages []string, pagesBefore map[string][]string) (bool) {
+    for index, page := range pages {
+        for i := 0; i < index; i++ {
+            if slices.Contains(pagesBefore[page], pages[i]) {
+                return false
+            }
+        }
+    }
+    return true
+}
